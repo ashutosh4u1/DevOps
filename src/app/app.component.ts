1811c4bdee3988e7bdc8415c94c29aeaf649ee31
ashutosh <ashutosh4u1@gmail.com>
2021-05-17 10:45:38 +0530

    Checkin 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CPTest';
+ 
+  //TEST 2 14/05/2021 14:52
+}
